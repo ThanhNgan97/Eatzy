6cd51f86b1ede36887f293927354c24fb7bd5556
Nguyễn Thư <anhthunguyen7890@gmail.com>
2025-07-02 14:25:18 +0700

    ShipperHome done

--- screens/Shipper/ShipperHomeScreen.jsx
+++ screens/Shipper/ShipperHomeScreen.jsx
@@ -1,15 +1,11 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ShipperHome from "../../components/Shipper/ShipperHome/ShipperHome";
 
 const ShipperHomeScreen = () => {
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.content}
-      keyboardShouldPersistTaps="handled"
-    >
+    <View style={styles.container}>
       <ShipperHome />
-    </ScrollView>
+    </View>
   );
 };
 
@@ -17,14 +13,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F4F8F7",
-  },
-
-  content: {
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
